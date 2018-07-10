221600845c6c552e2d46582b2265c09a59fb31ab
Scott Weinstein <sweinstein27@gmail.com>
2018-07-10 14:45:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,9 +18,9 @@ class Application
       if @@cart.empty?
         resp.write "cart is empty"
       else
-      @@cart.each do |item|
+        @@cart.each do |item|
         resp.write "#{item}\n"
-      end
+        end
       end
     else
       resp.write "Path Not Found"
