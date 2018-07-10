369eff45b421a3b5b79ecc9e358f85508ddf363c
Scott Weinstein <sweinstein27@gmail.com>
2018-07-10 14:43:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path(/cart/)
-      @@items.each do |item|
+      @@cart.each do |item|
         resp.write "#{item}\n"
       end
     else
