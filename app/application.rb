ea15e2c17214187efd7af7f8284377053a948892
Scott Weinstein <sweinstein27@gmail.com>
2018-07-10 15:33:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ class Application
           @@cart << item
           resp.write "added #{item}"
       else
-        resp.write "we don't have that item"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
