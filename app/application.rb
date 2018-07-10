fffee8b9f54f306c267a0698e18d07cb289f77ea
Scott Weinstein <sweinstein27@gmail.com>
2018-07-10 14:56:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,10 +27,10 @@ class Application
         @@items.each do |item|
         @@cart << item
         resp.write "added #{item}"
-      end
       else
         resp.write "we don't have that item"
       end
+    end
     end
 
     else
